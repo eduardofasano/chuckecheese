96507226e3dcc23616f36cbaa768526f719cd0f5
eduardofasano <eduardo.fasano.93@gmail.com>
2016-10-31 15:31:27 +0000

    working zoom functionality

--- public/js/app.js
+++ public/js/app.js
@@ -5,25 +5,27 @@ $(function () {
 
   var map = new google.maps.Map($mapDiv[0], {
     center: { lat: 42.77509, lng: 13.01239 },
-    zoom: 4
+    zoom: 3
   });
 
-  //CURRENT POSITION
-  navigator.geolocation.getCurrentPosition(function (position) {
-    var latLng = {
-      lat: position.coords.latitude,
-      lng: position.coords.longitude
-    };
-    map.panTo(latLng);
-    //map.setZoom(5);
+  console.log(map);
 
-    var maker = new google.maps.Marker({
-      position: latLng,
-      animation: google.maps.Animation.DROP,
-      draggable: true,
-      map: map
-    });
-  });
+  // //CURRENT POSITION
+  //   navigator.geolocation.getCurrentPosition((position) => {
+  //     let latLng = {
+  //       lat: position.coords.latitude,
+  //       lng:position.coords.longitude
+  //     };
+  //     map.panTo(latLng);
+  //     //map.setZoom(5);
+  //
+  //     let maker = new google.maps.Marker({
+  //       position: latLng,
+  //       animation: google.maps.Animation.DROP,
+  //       draggable: true,
+  //       map
+  //     });
+  //   });
 
   //POPULATE MAP
   var getEvents = $.get('http://eonet.sci.gsfc.nasa.gov/api/v2/events').done(function (data) {
@@ -57,13 +59,33 @@ $(function () {
   //ADD INFO WINDOW
   function addInfoWindowForDisaster(disaster, circle) {
     google.maps.event.addListener(circle, "click", function () {
-      console.log(circle);
-      console.log(disaster);
+      // console.log(circle);
+      // console.log(disaster);
       var infoWindow = new google.maps.InfoWindow({
         content: '\n      <h2>' + disaster.title + '</h2>',
         position: circle.center
       });
       infoWindow.open(map, circle);
+      map.setCenter(circle.center);
+      map.panTo(circle.center);
+      smoothZoom(map, 8, map.getZoom());
     });
+  }
+
+  //http://stackoverflow.com/questions/4752340/how-to-zoom-in-smoothly-on-a-marker-in-google-maps
+  function smoothZoom(map, max, cnt) {
+    if (cnt >= max) {
+      return;
+    } else {
+      (function () {
+        var z = google.maps.event.addListener(map, 'zoom_changed', function (event) {
+          google.maps.event.removeListener(z);
+          smoothZoom(map, max, cnt + 1);
+        });
+        setTimeout(function () {
+          map.setZoom(cnt);
+        }, 200);
+      })();
+    }
   }
 });
