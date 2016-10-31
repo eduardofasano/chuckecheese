0a4036acff3051f510f0e91dbeb4d84733bdb1cf
William Hilton <williamjchilton@gmail.com>
2016-10-31 15:30:01 +0000

    reg and log in form working  plus added filter form

--- public/js/app.js
+++ public/js/app.js
@@ -2,6 +2,7 @@
 
 $(function () {
   var $mapDiv = $('#map');
+  var circles = [];
 
   var map = new google.maps.Map($mapDiv[0], {
     center: { lat: 42.77509, lng: 13.01239 },
@@ -9,6 +10,7 @@ $(function () {
   });
 
   //CURRENT POSITION
+
   navigator.geolocation.getCurrentPosition(function (position) {
     var latLng = {
       lat: position.coords.latitude,
@@ -25,42 +27,97 @@ $(function () {
     });
   });
 
-  var getEvents = $.get('http://eonet.sci.gsfc.nasa.gov/api/v2/events').done(function (data) {
-    console.log(data);
-    data.events.forEach(function (disaster) {
-      if (disaster.geometries[0].coordinates[0] instanceof Array) {
-        (function () {
-          var bounds = new google.maps.LatLngBounds();
-          disaster.geometries[0].coordinates.forEach(function (coords) {
-            bounds.extend(new google.maps.LatLng(coords[1], coords[0]));
-          });
-          var circle = new google.maps.Circle({
-            center: bounds.getCenter(),
+  //POPULATE MAP
+  function populateMap() {
+    var getEvents = $.get('http://eonet.sci.gsfc.nasa.gov/api/v2/events').done(function (data) {
+      console.log(data);
+      data.events.forEach(function (disaster) {
+        if (disaster.geometries[0].coordinates[0] instanceof Array) {
+          (function () {
+            var bounds = new google.maps.LatLngBounds();
+            disaster.geometries[0].coordinates.forEach(function (coords) {
+              bounds.extend(new google.maps.LatLng(coords[1], coords[0]));
+            });
+            var circle = new google.maps.Circle({
+              center: bounds.getCenter(),
+              map: map,
+              radius: 500000,
+              fillColor: '#ff00ff'
+            });
+            circles.push(circle);
+          })();
+        } else {
+          var _circle = new google.maps.Circle({
+            center: new google.maps.LatLng(disaster.geometries[0].coordinates[1], disaster.geometries[0].coordinates[0]),
             map: map,
             radius: 500000,
             fillColor: '#ff00ff'
           });
-        })();
-      } else {
-        var _circle = new google.maps.Circle({
-          center: new google.maps.LatLng(disaster.geometries[0].coordinates[1], disaster.geometries[0].coordinates[0]),
-          map: map,
-          radius: 500000,
-          fillColor: '#ff00ff'
-        });
-      }
+          circles.push(_circle);
+        }
+      });
+      console.log(circles);
     });
-  });
+  }
 
-  // //RECTANGLE FUNCTIONALITY
-  // let rectangle = new google.maps.Rectangle({
-  //   strokeColor: '#FF0000',
-  //   strokeOpacity: 0.8,
-  //   strokeWeight: 2,
-  //   fillColor: '#ffffff',
-  //   fillOpacity: 0.35,
-  //   map: map,
-  //   bounds: bounds
-  // });
+  var $container = $('#container');
+  $container.on('submit', 'form', handleForm);
+  $container.on('click', '#logOut', logout);
 
+  //CREATE FORM
+  function isLoggedIn() {
+    return !!localStorage.getItem('token');
+  }
+
+  if (isLoggedIn()) {
+    showFilterForm();
+    populateMap();
+  } else {
+    showLoginForm();
+  }
+
+  function showLoginForm() {
+    if (event) event.preventDefault();
+    $container.html('\n      <div id="logInForm">\n      <form class="login" action="api/login" method="post">\n      <label for="email"></label>\n      <input type="text" name="email" placeholder="email" value="">\n      <label for="password"></label>\n      <input type="password" name="password" placeholder="password" value="">\n      <input type="submit" name="Log in" value="Log in" class=\'button\'><br>\n      </form>\n      </div>\n      <br><br><br>\n      <div id="registerForm">\n      <form class="register" action="api/register" method="post">\n      <label for="username"></label>\n      <input type="text" name="username" placeholder="username" value="">\n      <label for="email"></label>\n      <input type="text" name="email" placeholder="email" value="">\n      <label for="password"></label>\n      <input type="password" name="password" placeholder="password" value="">\n      <label for="passwordConfirmation"></label>\n      <input type="password" name="passwordConfirmation" placeholder="password confirmation" value="">\n      <input type="submit" name="register" value="Register" class=\'button\'><br>\n      </form>\n      </div>\n      ');
+  }
+
+  function handleForm() {
+    if (event) event.preventDefault();
+    var token = localStorage.getItem('token');
+    var $form = $(this);
+
+    var url = $form.attr('action');
+    var method = $form.attr('method');
+    var data = $form.serialize();
+
+    //LOGGING IN & REGISTRATION
+    $.ajax({
+      url: url,
+      method: method,
+      data: data,
+      beforeSend: function beforeSend(jqXHR) {
+        if (token) return jqXHR.setRequestHeader('Authorization', 'Bearer ' + token);
+      }
+    }).done(function (data) {
+      if (data.token) localStorage.setItem("token", data.token);
+      console.log('hello');
+      showFilterForm();
+      populateMap();
+    });
+  }
+
+  function showFilterForm() {
+    if (event) event.preventDefault();
+    $container.html('\n        <form class="filter" action="#" method="get">\n        <input type="checkbox" name="drought" value="Drought">Drought\n        <input type="checkbox" name="dustAndHaze" value="Dust and Haze">Dust and Haze\n        <input type="checkbox" name="wildfires" value="Wildfires">Wildfires\n        <input type="checkbox" name="floods" value="Floods">Floods\n        <input type="checkbox" name="severeStorms" value="Severe Storms">Severe Storms\n        <input type="checkbox" name="volcanoes" value="Volcanoes">Volcanoes\n        <input type="checkbox" name="waterColor" value="Water Color">Water Color\n        <input type="checkbox" name="landslides" value="Landslides">Landslides\n        <input type="checkbox" name="seaLakeIce" value="Sea Lake Ice">Sea Lake Ice\n        <input type="checkbox" name="earthquakes" value="Earthquakes">Earthquakes\n        <input type="checkbox" name="snow" value="Snow">Snow\n        <input type="checkbox" name="temperatureExtreme" value="Temperature Extreme">Temperature Extreme\n        <input type="checkbox" name="manMade" value="Man Made">Man Made\n        <button>Filter</button>\n        <button id="logOut">Log Out</button>\n        </form>\n        ');
+  }
+
+  function logout() {
+    if (event) event.preventDefault();
+    localStorage.removeItem('token');
+    showLoginForm();
+    circles.forEach(function (circle) {
+      circle.setMap(null);
+    });
+    circles = [];
+  }
 });
--- src/js/app.js
+++ src/js/app.js
@@ -1,70 +1,177 @@
 $(() => {
   let $mapDiv = $('#map');
+  let circles = [];
 
-  let map = new google.maps.Map($mapDiv[0], {
-    center: { lat: 42.77509, lng: 13.01239 },
-    zoom: 4
-  });
-
-//CURRENT POSITION
-  navigator.geolocation.getCurrentPosition((position) => {
-    let latLng = {
-      lat: position.coords.latitude,
-      lng:position.coords.longitude
-    };
-    map.panTo(latLng);
-    //map.setZoom(5);
-
-    let maker = new google.maps.Marker({
-      position: latLng,
-      animation: google.maps.Animation.DROP,
-      draggable: true,
-      map
+    let map = new google.maps.Map($mapDiv[0], {
+      center: { lat: 42.77509, lng: 13.01239 },
+      zoom: 4
     });
-  });
 
-  let getEvents = $.get('http://eonet.sci.gsfc.nasa.gov/api/v2/events')
-  .done(function(data) {
-    console.log(data);
-    data.events.forEach((disaster) => {
-      if(disaster.geometries[0].coordinates[0] instanceof Array) {
-        let bounds = new google.maps.LatLngBounds();
-        disaster.geometries[0].coordinates.forEach((coords) => {
-          bounds.extend(new google.maps.LatLng(coords[1], coords[0]));
-        });
-        let circle = new google.maps.Circle({
-          center: bounds.getCenter(),
-          map: map,
-          radius: 500000,
-          fillColor: '#ff00ff'
-        });
-      } else {
-        let circle = new google.maps.Circle({
-          center: new google.maps.LatLng(disaster.geometries[0].coordinates[1], disaster.geometries[0].coordinates[0]),
-          map: map,
-          radius: 500000,
-          fillColor: '#ff00ff'
-        });
+    //CURRENT POSITION
+
+    navigator.geolocation.getCurrentPosition((position) => {
+      let latLng = {
+        lat: position.coords.latitude,
+        lng:position.coords.longitude
+      };
+      map.panTo(latLng);
+      //map.setZoom(5);
+
+      let maker = new google.maps.Marker({
+        position: latLng,
+        animation: google.maps.Animation.DROP,
+        draggable: true,
+        map
+      });
+    });
+
+
+
+  //POPULATE MAP
+  function populateMap() {
+    let getEvents = $.get('http://eonet.sci.gsfc.nasa.gov/api/v2/events')
+    .done(function(data) {
+      console.log(data);
+      data.events.forEach((disaster) => {
+        if(disaster.geometries[0].coordinates[0] instanceof Array) {
+          let bounds = new google.maps.LatLngBounds();
+          disaster.geometries[0].coordinates.forEach((coords) => {
+            bounds.extend(new google.maps.LatLng(coords[1], coords[0]));
+          });
+          let circle = new google.maps.Circle({
+            center: bounds.getCenter(),
+            map: map,
+            radius: 500000,
+            fillColor: '#ff00ff'
+          });
+          circles.push(circle);
+        } else {
+          let circle = new google.maps.Circle({
+            center: new google.maps.LatLng(disaster.geometries[0].coordinates[1], disaster.geometries[0].coordinates[0]),
+            map: map,
+            radius: 500000,
+            fillColor: '#ff00ff'
+          });
+          circles.push(circle);
+        }
+      });
+      console.log(circles);
+    });
+  }
+
+  let $container = $('#container');
+  $container.on('submit', 'form', handleForm);
+  $container.on('click', '#logOut', logout);
+
+
+  //CREATE FORM
+  function isLoggedIn() {
+    return !!localStorage.getItem('token');
+  }
+
+  if(isLoggedIn()) {
+    showFilterForm();
+    populateMap();
+  } else {
+    showLoginForm();
+  }
+
+  function showLoginForm() {
+    if (event) event.preventDefault();
+    $container.html(`
+      <div id="logInForm">
+      <form class="login" action="api/login" method="post">
+      <label for="email"></label>
+      <input type="text" name="email" placeholder="email" value="">
+      <label for="password"></label>
+      <input type="password" name="password" placeholder="password" value="">
+      <input type="submit" name="Log in" value="Log in" class='button'><br>
+      </form>
+      </div>
+      <br><br><br>
+      <div id="registerForm">
+      <form class="register" action="api/register" method="post">
+      <label for="username"></label>
+      <input type="text" name="username" placeholder="username" value="">
+      <label for="email"></label>
+      <input type="text" name="email" placeholder="email" value="">
+      <label for="password"></label>
+      <input type="password" name="password" placeholder="password" value="">
+      <label for="passwordConfirmation"></label>
+      <input type="password" name="passwordConfirmation" placeholder="password confirmation" value="">
+      <input type="submit" name="register" value="Register" class='button'><br>
+      </form>
+      </div>
+      `);
+    }
+
+    function handleForm() {
+      if(event) event.preventDefault();
+      let token = localStorage.getItem('token');
+      let $form = $(this);
+
+      let url = $form.attr('action');
+      let method = $form.attr('method');
+      let data = $form.serialize();
+
+      //LOGGING IN & REGISTRATION
+      $.ajax({
+        url,
+        method,
+        data,
+        beforeSend: function(jqXHR) {
+          if(token) return jqXHR.setRequestHeader('Authorization', `Bearer ${token}`);
+        }
+      }).done((data) => {
+        if(data.token) localStorage.setItem("token", data.token);
+        console.log('hello');
+        showFilterForm();
+        populateMap();
+      });
+    }
+
+    function showFilterForm() {
+      if (event) event.preventDefault();
+      $container.html(`
+        <form class="filter" action="#" method="get">
+        <input type="checkbox" name="drought" value="Drought">Drought
+        <input type="checkbox" name="dustAndHaze" value="Dust and Haze">Dust and Haze
+        <input type="checkbox" name="wildfires" value="Wildfires">Wildfires
+        <input type="checkbox" name="floods" value="Floods">Floods
+        <input type="checkbox" name="severeStorms" value="Severe Storms">Severe Storms
+        <input type="checkbox" name="volcanoes" value="Volcanoes">Volcanoes
+        <input type="checkbox" name="waterColor" value="Water Color">Water Color
+        <input type="checkbox" name="landslides" value="Landslides">Landslides
+        <input type="checkbox" name="seaLakeIce" value="Sea Lake Ice">Sea Lake Ice
+        <input type="checkbox" name="earthquakes" value="Earthquakes">Earthquakes
+        <input type="checkbox" name="snow" value="Snow">Snow
+        <input type="checkbox" name="temperatureExtreme" value="Temperature Extreme">Temperature Extreme
+        <input type="checkbox" name="manMade" value="Man Made">Man Made
+        <button>Filter</button>
+        <button id="logOut">Log Out</button>
+        </form>
+        `);
       }
+
+      function logout() {
+        if(event) event.preventDefault();
+        localStorage.removeItem('token');
+        showLoginForm();
+        circles.forEach((circle) => {
+          circle.setMap(null);
+        });
+        circles = [];
+      }
+
+
+
+
+
+
+
+
+
+
+
+
     });
-});
-
-// //RECTANGLE FUNCTIONALITY
-// let rectangle = new google.maps.Rectangle({
-//   strokeColor: '#FF0000',
-//   strokeOpacity: 0.8,
-//   strokeWeight: 2,
-//   fillColor: '#ffffff',
-//   fillOpacity: 0.35,
-//   map: map,
-//   bounds: bounds
-// });
-
-
-
-
-
-
-
-
-});
